d85ceeaabdc9e552c1420472d135e1fe093d7f96
Thai Thanh Hai <thaithanhhaick@gmail.com>
2021-12-20 23:45:23 +0700

    second commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+// app.use(morgan('combined'));
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const hbs = handlebars.create({
+  extname: '.hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', './views');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
