2abfcab080f2805192ed607f44dc718214b95064
Thai Thanh Hai <thaithanhhaick@gmail.com>
2021-12-21 13:27:11 +0700

    third commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+// HTTP logger
 // app.use(morgan('combined'));
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -13,13 +14,36 @@ const hbs = handlebars.create({
   extname: '.hbs'
 });
 
+// Template engine
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', './views');
+
 app.set('views', path.join(__dirname, 'resources/views'));
 
+// 
+app.use(express.json())
+app.use(express.urlencoded()) 
+
+// [GET] home
 app.get('/', (req, res) => {
+  res.render('home');
+})
+
+// [GET] news
+app.get('/news', (req, res) => {
   res.render('news');
+})
+
+// [GET] search
+app.get('/search', (req, res) => {
+  res.render('search');
+})
+
+// [POST] search
+app.post('/search', (req, res) => {
+  console.log(req.body);
+  res.send('AAAAA');
 })
 
 app.listen(port, () => {
