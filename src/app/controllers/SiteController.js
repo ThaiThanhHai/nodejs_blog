6cefd23cf66724527792509f773741170f6673b4
Thai Thanh Hai <thaithanhhaick@gmail.com>
2021-12-21 23:06:11 +0700

    lan thu 6

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,19 +1,17 @@
 const Course = require('../models/Course');
-
+const { multipleMongooseToObject } = require('../../util/mongoose');
+ 
 class SiteController {
     // [GET] /home
-    home(req, res) {
-
-        Course.find({}, (err, courses) => {
-            if (!err) {
-                res.json(courses);
-                return;
-            }
-            res.status(400).json({ error: 'ERROR!!!' });
-        });
-
-
-        // res.render('home');
+    home(req, res, next) {
+        Course.find({})
+            .then((courses) => {
+                res.render('home', { 
+                    courses: multipleMongooseToObject(courses)
+                })
+            })
+            .catch(next);
+        
     }
 
     // [GET] /search
